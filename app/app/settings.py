3b102fe4f5da0bc8ed655e7c3bcdada7ea7dce11
Materia597 <sethj.browning@calbaptist.edu>
2025-11-11 23:12:44 -0800

    Added the core app for the main landing pages of the site.

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'forum.apps.ForumConfig'
+    'forum.apps.ForumConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
