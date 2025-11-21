48f47da72df76db8e4f6d83072b3980bc9fde8f8
Materia597 <sethj.browning@calbaptist.edu>
2025-11-20 20:16:45 -0800

    Updated the register form styles to be legible.

--- app/accounts/static/accounts/js/register.js
+++ app/accounts/static/accounts/js/register.js
@@ -1,13 +1,4 @@
-const registerForm = document.getElementById('register-form');
 
-
-registerForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const username = document.getElementById('username-input').value.trim()
-    console.log(username)
-
-    localStorage.setItem('loggedInUser', username)
-
-    window.location.href = "home.html"
+document.querySelectorAll('.helptext').forEach(helpText => {
+    helpText.insertAdjacentHTML('beforebegin', '<br>')
 })
