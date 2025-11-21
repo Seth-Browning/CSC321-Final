461e74855283172f8acc8c2e3ced24511881982a
Materia597 <sethj.browning@calbaptist.edu>
2025-11-20 22:16:05 -0800

    Changed the nav-bar so that it reflects the user's logged in state.
    All of the current html files have been modified to reflect this change.

--- app/static/app/js/navbar.js
+++ app/static/app/js/navbar.js
@@ -4,38 +4,52 @@
  * of the website.
  */
 class NavBar extends HTMLElement {
+    static observedAttributes = [
+        'logged-in',
+        'username',
+        'home-link',
+        'login-link',
+        'register-link'
+    ]
+
     constructor() {
         super();
     }
 
     connectedCallback() {
         
+        const loggedIn = this.getAttribute('logged-in') ?? 'false'
+        const username = this.getAttribute('username') ?? ''
+        const homeLink = this.getAttribute('home-link') ?? '/'
+        const loginLink = this.getAttribute('login-link') ?? '/'
+        const registerLink = this.getAttribute('register-link') ?? '/'
+
+
         this.innerHTML = `
             <nav>
                 <div id="nav-bar-left">
                     <p class="home-page-link">
-                        <a href="./home.html">Games Forum</a>
+                        <a href="${homeLink}">Games Forum</a>
                     </p>
                 </div>
-
-                <div id="log-in-options" class="nav-log-dependent" data-active="true">
-                    <button class="primary-button" id="to-login">Log In</button>
-                    <button class="secondary-button" id="to-signup">Sign Up</button>
-                </div>
-
-                <div id="logged-in-options" class="nav-log-dependent" data-active="false">
+                ${ loggedIn === 'True' ? `<div id="logged-in-options" class="nav-log-dependent" data-active="false">
                     <svg class="small-icon"><use href="../assets/icons/icons.svg#bell"></use></svg>
                     <svg class="icon"><use href="../assets/icons/icons.svg#circle-user"></use></svg>
-                </div>
-            </nav>
-        `;
+                </div>` : `<div id="log-in-options" class="nav-log-dependent" data-active="true">
+                    <button class="primary-button" id="to-login">Log In</button>
+                    <button class="secondary-button" id="to-signup">Sign Up</button>
+                </div>` }
+            </nav>`
+
+        console.log(this.innerHTML)
+
 
         this.querySelector('#to-login').addEventListener('click', () => {
-            window.location.href = "login.html"
+            window.location.href = loginLink
         })
 
         this.querySelector('#to-signup').addEventListener('click', () => {
-            window.location.href = "register.html"
+            window.location.href = registerLink
         })
     }
 }
