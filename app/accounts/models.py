ddea48d50fb4302c2b2f3d50d3e655056006afce
Seth Browning <sethj.browning@calbaptist.edu>
2025-11-27 13:57:31 -0800

    Changed the Profile model so that followers are now a possible feature.

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    following = models.ManyToManyField('self', symmetrical=False, related_name="followers", blank=True)
+
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
