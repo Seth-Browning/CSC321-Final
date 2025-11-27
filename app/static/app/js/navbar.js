edee1d7babf45d79f32112cb4af5835f90697cc2
Seth Browning <sethj.browning@calbaptist.edu>
2025-11-27 13:35:27 -0800

    Modified the navbar so that is shows the username when logged in.
    Clicking the username also takes the user to their profile page, in the future it could be changed to a dedicated settings page.

--- app/static/app/js/navbar.js
+++ app/static/app/js/navbar.js
@@ -9,7 +9,7 @@ class NavBar extends HTMLElement {
         'username',
         'home-link',
         'login-link',
-        'register-link'
+        'register-link',
     ]
 
     constructor() {
@@ -18,12 +18,29 @@ class NavBar extends HTMLElement {
 
     connectedCallback() {
         
-        const loggedIn = this.getAttribute('logged-in') ?? 'false'
+        const loggedIn = this.getAttribute('logged-in') ?? 'False'
         const username = this.getAttribute('username') ?? ''
+
         const homeLink = this.getAttribute('home-link') ?? '/'
         const loginLink = this.getAttribute('login-link') ?? '/'
         const registerLink = this.getAttribute('register-link') ?? '/'
 
+        let navBarOptions;
+
+        // the loggedIn property is set by the renderer in python, so the boolean as a string
+        // will be capitalized instead of lowercase
+        if (loggedIn === "True") {
+            navBarOptions = `
+            <div id="logged-in-options" class="nav-log-dependent" data-active="true">
+                <p class="logged-in-username">${username}</p>
+            </div>` 
+        } else {
+            navBarOptions = `
+            <div id="log-in-options" class="nav-log-dependent" data-active="true">
+                <button class="primary-button to-login" >Log In</button>
+                <button class="secondary-button to-signup">Sign Up</button>
+            </div>`
+        }
 
         this.innerHTML = `
             <nav>
@@ -32,22 +49,20 @@ class NavBar extends HTMLElement {
                         <a href="${homeLink}">Games Forum</a>
                     </p>
                 </div>
-                ${ loggedIn === 'True' ? `<div id="logged-in-options" class="nav-log-dependent" data-active="false">
-                    <svg class="small-icon"><use href="../assets/icons/icons.svg#bell"></use></svg>
-                    <svg class="icon"><use href="../assets/icons/icons.svg#circle-user"></use></svg>
-                </div>` : `<div id="log-in-options" class="nav-log-dependent" data-active="true">
-                    <button class="primary-button" id="to-login">Log In</button>
-                    <button class="secondary-button" id="to-signup">Sign Up</button>
-                </div>` }
+                ${ navBarOptions }
             </nav>`
 
 
-        this.querySelector('#to-login').addEventListener('click', () => {
+        this.querySelector('.to-login')?.addEventListener('click', () => {
             window.location.href = loginLink
         })
 
-        this.querySelector('#to-signup').addEventListener('click', () => {
+        this.querySelector('.to-signup')?.addEventListener('click', () => {
             window.location.href = registerLink
+        })
+
+        this.querySelector('.logged-in-username')?.addEventListener('click', () => {
+            window.location.href = `/accounts/profile/${username}/`
         })
     }
 }
